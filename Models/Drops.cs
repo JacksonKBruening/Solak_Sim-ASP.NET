96f4fbce967e458b1f7aaf102742217aa80ec329
Jackson Bruening <85266719+JacksonKBruening@users.noreply.github.com>
2021-06-20 18:42:48 -0500

    Add files via upload

---
+++ Models/Drops.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Solak_Simulator.Models
+{
+    public class Drops
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+
+        public string Rarity { get; set; }
+
+        public string Range { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Drops>(this);
+
+    }
+}
---
+++ Services/JsonFileDropServices.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Solak_Simulator.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Solak_Simulator.Services
+{
+    public class JsonFileDropServices
+    {
+       
+        
+            public JsonFileDropServices(IWebHostEnvironment webHostEnvironment)
+            {
+                WebHostEnvironment = webHostEnvironment;
+            }
+
+            public IWebHostEnvironment WebHostEnvironment { get; }
+
+            private string JsonFileName
+            {
+                get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "comms.json"); }
+            }
+
+            public IEnumerable<Drops> GetDrops()
+            {
+                using (var jsonFileReader = File.OpenText(JsonFileName))
+                {
+                    return JsonSerializer.Deserialize<Drops[]>(jsonFileReader.ReadToEnd(),
+                        new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true
+                        });
+                }
+            }
+        
+    }
+}
